b2b1ccecd5be5bc9a3401ba066fef18f057c194f
Yan Hua <yhua219@UoA.auckland.ac.nz>
2018-11-23 15:13:23 +1300

    Modified to be called by RunExperiment

--- src/main/java/org/kramerlab/bmad/algorithms/LocalSearch.java
+++ src/main/java/org/kramerlab/bmad/algorithms/LocalSearch.java
@@ -1,8 +1,11 @@
 package org.kramerlab.bmad.algorithms;
 
 import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.kramerlab.bmad.general.Tuple;
 import org.kramerlab.bmad.matrix.BooleanMatrix;
 import org.kramerlab.bmad.matrix.RandomMatrixGeneration;
+import weka.core.Instances;
+import java.util.*;
 
 
 /**
@@ -16,24 +19,118 @@ public class LocalSearch {
 
     private BooleanMatrix C;
     private int k;
-    public boolean nextDescent;
 
-    public LocalSearch(BooleanMatrix a,int dimension, boolean nextDescent ) {
-        super();
+    // Constructor - takes a BooleanMatrix object
+    public LocalSearch(BooleanMatrix a,int dimension) {
         this.C = a;
         this.k = dimension;
     }
 
-    public void decompose(){
-
+    // Overloaded constructor, takes a Weka Instances object.
+    public LocalSearch(Instances instanceA, int dimension) {
+        this(new BooleanMatrix(instanceA), dimension);
     }
 
-    // Take any old descent direction.
-    public double nextDescent(){
+
+    /**
+     * Main portal for performing decomposition.
+     * Calls one of the three algorithms using either OR or XOR with booleanProduct.
+     *
+     * @param algorithm: 1, use nextDecent;  2, use steepDescent; 3, use randomRestarts.
+     * @param numRestarts: for algorithm 3 randomRestarts only.
+     * @param xor: true: booleanProduct uses XOR;  false: booleanProduct uses OR.
+     * @return double relativeDeconstructionError.
+     */
+//
+    public double decompose(int algorithm, int numRestarts, boolean nextDescent, boolean xor){
+        try{
+            if(algorithm == 1){
+                return nextDescent(xor);
+            }else if (algorithm == 2){
+                return steepestDescent(xor);
+            }else if (algorithm == 3) {
+                return randomRestarts(numRestarts, nextDescent, xor);
+            }
+        }catch(Exception e){
+            System.out.println("WARNING: There are only 3 algorithms: 1 = nextDescent, 2 = steepDescent, 3 = randomRestarts.");
+            }
+        return -999;
+    }
+
+    /**
+     * Overloadd decompose method for calling nextDescent and steepDescent with only two parameters.
+     */
+
+    public double decompose(int algorithm, boolean xor){
+        try{
+            if(algorithm == 1){
+                return nextDescent(xor);
+            }else if(algorithm == 2){
+                return steepestDescent(xor);
+            }
+        }catch(Exception e){
+            System.out.println("WARNING: This version of decomposition method only calls two algorithms: 1 = nextDescent, 2 = steepDescent.");
+        }
+        return -999;
+    }
+
+
+
+    /**
+     * Calls the decompose method while calculating the runtime.
+     * @return a tuple of <(double) relativeReconstructionError, (long) runtime>.
+     */
+
+    public Tuple<Double, Long> decomposeWithRuntime(int algorithm, int numRestarts, boolean nextDescent, boolean xor){
+        double relativeReconError;
+
+        long startTime = System.nanoTime();
+
+        relativeReconError = decompose(algorithm, numRestarts, nextDescent, xor);
+
+        long stopTime = System.nanoTime();
+        long runTime = stopTime - startTime;
+
+        Tuple<Double, Long> result = new Tuple<Double, Long>(relativeReconError, runTime);
+        return result;
+    }
+
+    /**
+     * Overloaded decomposeWithRuntime that only calls nextDescent and steepDescent.
+     * @return a tuple of <(double) relativeReconstructionError, (long) runtime>.
+     */
+
+    public Tuple<Double, Long> decomposeWithRuntime(int algorithm, boolean xor){
+        double relativeReconError;
+
+        long startTime = System.nanoTime();
+
+        relativeReconError = decompose(algorithm, xor);
+
+        long stopTime = System.nanoTime();
+        long runTime = stopTime - startTime;
+
+        Tuple<Double, Long> result = new Tuple<Double, Long>(relativeReconError, runTime);
+        return result;
+    }
+
+
+
+    /**
+     *  Hill-climbing method 1 - Take any old descent direction.
+     *  Returns a (double) relativeReconstructionError.
+     *  @param xor: boolean indicator for calling booleanProduct with XOR vs. OR.
+     */
+
+    public double nextDescent(boolean xor){
+
+//        System.out.printf("%n--------------------Running nextDescent with %s--------------------%n", xor? "XOR": "OR");
 
         int n = this.C.getHeight();
         int m = this.C.getWidth();
         int it = 0;
+        double relativeRecError;
+
         boolean improved = false;
         double density = this.C.getDensity();
         BooleanMatrix C_T = BooleanMatrix.deepTranspose(this.C);
@@ -44,31 +141,31 @@ public class LocalSearch {
         BooleanMatrix B = RandomMatrixGeneration.randomMatrix(this.k,m,density,0d);
 
         // Print out the initial guess and its recon error.
-        BooleanMatrix randomResult = S.booleanProduct(B);
+        BooleanMatrix randomResult = S.booleanProduct(B, xor);
         double randomError = this.C.reconstructionError(randomResult,1d);
-        //System.out.print("The initial recon-error is: " + Double.toString(randomError));
+//        System.out.println("The initial recon-error is: " + randomError);
 
 
         while(true){
 
             // Explore neighbourhood of 1 bit swaps of S
             improved = false; // keep track of if an improvement has been made in this neighbourhood
-            for (int i =0;i<n;i++){
+            for (int i = 0; i < n; i++){
 
                 BooleanMatrix S_i = S.getRow(i);// still points to the same row
-                BooleanMatrix incumbentRow = S_i.booleanProduct(B);
+                BooleanMatrix incumbentRow = S_i.booleanProduct(B, xor);
                 double incumbentRowError = this.C.getRow(i).reconstructionError(incumbentRow,1d);
 
-                for (int j =0;j<this.k;j++){
+                for (int j = 0; j < this.k; j++){
 
-                    S_i.update(j,BooleanMatrix.not(S_i.apply(j))); // flip bit.
-                    BooleanMatrix rowResult = S_i.booleanProduct(B);
+                    S_i.update(j, BooleanMatrix.not(S_i.apply(j))); // flip bit.
+                    BooleanMatrix rowResult = S_i.booleanProduct(B, xor);
                     double rowError = this.C.getRow(i).reconstructionError(rowResult,1d);
 
                     if (rowError < incumbentRowError){
                         improved = true;
                     }else{
-                        S_i.update(j,BooleanMatrix.not(S_i.apply(j))); // undo the change.
+                        S_i.update(j, BooleanMatrix.not(S_i.apply(j))); // undo the change.
                     }
                 }
             }
@@ -76,17 +173,17 @@ public class LocalSearch {
             BooleanMatrix S_T = BooleanMatrix.deepTranspose(S);
             BooleanMatrix B_T = BooleanMatrix.deepTranspose(B);
 
-            for (int i =0;i<m;i++){
+            for (int i = 0; i < m; i++){
 
                 BooleanMatrix B_Ti = B_T.getRow(i);// still points to the same row
-                BooleanMatrix incumbentRow = B_Ti.booleanProduct(S_T);
+                BooleanMatrix incumbentRow = B_Ti.booleanProduct(S_T, xor);
 
                 double incumbentRowError = C_T.getRow(i).reconstructionError(incumbentRow,1d);
 
-                for (int j =0;j<this.k;j++){
+                for (int j = 0; j < this.k; j++){
 
                     B_Ti.update(j,BooleanMatrix.not(B_Ti.apply(j))); // flip bit.
-                    BooleanMatrix rowResult = B_Ti.booleanProduct(S_T);
+                    BooleanMatrix rowResult = B_Ti.booleanProduct(S_T, xor);
                     double rowError = C_T.getRow(i).reconstructionError(rowResult,1d);
 
                     if (rowError < incumbentRowError){
@@ -102,25 +199,36 @@ public class LocalSearch {
 
 //            if(it%10==0) {
 //                System.out.println("The recon-error is: " +
-//                        Double.toString(this.C.relativeReconstructionError(S.booleanProduct(B), 1d)));
+//                        Double.toString(this.C.relativeReconstructionError(S.booleanProduct(B, xor), 1d)));
 //            }
             it = it +1;
 
             // stop when our solution is locally optimal wrt to this neighbourhood.
             if (!improved){
-                //System.out.println("The final-recon error is: " + Double.toString(this.C.relativeReconstructionError(S.booleanProduct(B),1d)));
+                relativeRecError = this.C.relativeReconstructionError(S.booleanProduct(B, xor),1d);
+//                System.out.println("The final-recon error is: " + relativeRecError);
                 break;
             }
-
         }
-
-        return this.C.relativeReconstructionError(S.booleanProduct(B),1d);
+        return relativeRecError;
     }
 
-    // Find the best descent direction.
-    public void steepestDescent(){
+
+
+
+    /**
+     *  Hill-climbing method 2 - Find the best descent direction.
+     *  Returns a (double) relativeReconstructionError.
+     * @param xor: boolean indicator for calling booleanProduct with XOR vs. OR.
+     */
+
+    public double steepestDescent(boolean xor){
+
+//        System.out.printf("%n--------------------Running steepDescent with %s--------------------%n", xor? "XOR": "OR");
+
         int n = this.C.getHeight();
         int m = this.C.getWidth();
+        double relativeRecError;
 
         double density = this.C.getDensity();
         BooleanMatrix C_T = BooleanMatrix.deepTranspose(this.C);
@@ -131,9 +239,9 @@ public class LocalSearch {
         BooleanMatrix B = RandomMatrixGeneration.randomMatrix(this.k,m,density,0d);
 
         // Print out the initial guess and its recon error.
-        BooleanMatrix randomResult = S.booleanProduct(B);
+        BooleanMatrix randomResult = S.booleanProduct(B, xor);
         double randomError = this.C.reconstructionError(randomResult,1d);
-        System.out.print("The initial recon-error is: " + Double.toString(randomError));
+//        System.out.println("The initial recon-error is: " + randomError);
 
         int best_i;
         int best_j;
@@ -148,16 +256,16 @@ public class LocalSearch {
             incumbentErrorDelta = 0d;
 
             // Explore neighbourhood of 1 bit swaps of S
-            for (int i =0;i<n;i++){
+            for (int i = 0; i < n; i++){
 
                 BooleanMatrix S_i = S.getRow(i);// still points to the same row
-                BooleanMatrix incumbentRow = S_i.booleanProduct(B);
+                BooleanMatrix incumbentRow = S_i.booleanProduct(B, xor);
                 double incumbentRowError = this.C.getRow(i).reconstructionError(incumbentRow,1d);
 
-                for (int j =0;j<this.k;j++){
+                for (int j = 0; j < this.k; j++){
 
                     S_i.update(j,BooleanMatrix.not(S_i.apply(j))); // flip bit.
-                    BooleanMatrix rowResult = S_i.booleanProduct(B);
+                    BooleanMatrix rowResult = S_i.booleanProduct(B,xor);
                     double rowError = this.C.getRow(i).reconstructionError(rowResult,1d);
 
                     if (rowError - incumbentRowError < incumbentErrorDelta){
@@ -168,24 +276,23 @@ public class LocalSearch {
                     }
 
                     S_i.update(j,BooleanMatrix.not(S_i.apply(j)));
-
                 }
             }
 
             BooleanMatrix S_T = BooleanMatrix.deepTranspose(S);
             BooleanMatrix B_T = BooleanMatrix.deepTranspose(B);
 
-            for (int i =0;i<m;i++){
+            for (int i = 0; i < m; i++){
 
                 BooleanMatrix B_Ti = B_T.getRow(i);// still points to the same row
-                BooleanMatrix incumbentRow = B_Ti.booleanProduct(S_T);
+                BooleanMatrix incumbentRow = B_Ti.booleanProduct(S_T, xor);
 
                 double incumbentRowError = C_T.getRow(i).reconstructionError(incumbentRow,1d);
 
-                for (int j =0;j<this.k;j++){
+                for (int j = 0; j < this.k; j++){
 
                     B_Ti.update(j,BooleanMatrix.not(B_Ti.apply(j))); // flip bit.
-                    BooleanMatrix rowResult = B_Ti.booleanProduct(S_T);
+                    BooleanMatrix rowResult = B_Ti.booleanProduct(S_T, xor);
                     double rowError = C_T.getRow(i).reconstructionError(rowResult,1d);
 
                     if (rowError - incumbentRowError < incumbentErrorDelta){
@@ -201,38 +308,57 @@ public class LocalSearch {
 
             it = it +1;
 
-            if(it%100==0) {
-                System.out.println("The current recon-error is: " +
-                        Double.toString(this.C.relativeReconstructionError(S.booleanProduct(B), 1d)));
+            if(it % 100 == 0) {
+                relativeRecError = this.C.relativeReconstructionError(S.booleanProduct(B, xor), 1d);
+//                System.out.println("The current recon-error is: " + relativeRecError);
+
             }
 
-            if (!(best_i<0)){
-                if(flipS==true){
+            if (!(best_i < 0)){
+                if(flipS == true){
                     S.update(best_i,best_j,BooleanMatrix.not(S.apply(best_i,best_j)));
                 }else{
                     B.update(best_i,best_j,BooleanMatrix.not(B.apply(best_i,best_j)));
                 }
             }else{
-                System.out.println("The final-recon error is: " + Double.toString(this.C.relativeReconstructionError(S.booleanProduct(B),1d)));
+                relativeRecError = this.C.relativeReconstructionError(S.booleanProduct(B, xor), 1d);
+//                System.out.println("The final-recon error is: " + relativeRecError);
+
                 break;
             }
-
         }
-
-
+        return relativeRecError;
     }
 
-    // Try different initial conditions to find better local minima.
-    public void randomRestarts(int numRestarts){
+
+    /**
+     * Try different initial conditions to find better local minima.
+     * @param numRestarts
+     * @param xor: true = use XOR in booleanProduct, false = use OR in booleanProduct
+     */
+
+    public double randomRestarts(int numRestarts, boolean nextDescent, boolean xor){
+
+//        System.out.printf("%n--------------------Running randomRestarts with %s, %s--------------------%n", xor? "XOR": "OR",  nextDescent? "nextDescent": "steepDescent");
+
         double best_recon = 1;
+        double r;
         for (int i = 0; i < numRestarts; i++) {
-            double r = this.nextDescent();
-            if (r<best_recon){
+            if(nextDescent) {
+                r = this.nextDescent(xor);
+            }else{
+                r = this.steepestDescent(xor);
+            }
+            if (r < best_recon){
                 best_recon = r;
             }
         }
 
-        System.out.println(best_recon);
+
+//        System.out.printf("Using %s, %s, Best_recon = %f%n",  xor? "XOR": " OR", nextDescent? "nextDescent": "steepDescent", best_recon);
+        return best_recon;
     }
+
+
 
 }
